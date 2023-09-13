c53678591c7e62fb8dcaf9037650ed58f8227c87
Abhinav-kak10 <88281326+Abhinav-kak10@users.noreply.github.com>
2023-09-13 15:33:31 +0530

    Updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,3 @@
-// import Product from "./pages/Product";
-// import Home from "./pages/Home";
-// import ProductList from "./pages/ProductList";
-// import Register from "./pages/Register";
-// import Login from "./pages/Login";
-// import Cart from "./pages/Cart";
-
-// const App = () => {
-//   return <Login/>;
-// };
-
-// export default App;
-
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Product from "./pages/Product";
 import Home from "./pages/Home";
